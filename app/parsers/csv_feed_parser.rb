2b1afb2490ee41a78e9cce555cdd9f510e332fde
dimanyc <dimitrynazarov@gmail.com>
2017-01-10 11:38:56 -0500

    adds file exporter func to CSVFeedParser

--- app/parsers/csv_feed_parser.rb
+++ app/parsers/csv_feed_parser.rb
@@ -6,7 +6,7 @@ require 'csv'
 class CSVFeedParser
 
   def self.fetch_skus(file)
-   skus = Array.new
+    skus = Array.new
     CSV.foreach(file) do |row|
       skus.push(row.first)
     end
@@ -33,10 +33,28 @@ class CSVFeedParser
     nodes
   end
 
-  def self.compare_nodes(a, b)
-    a_nodes = fetch_nodes(a)
-    b_nodes = fetch_nodes(b)
-    binding.pry
+  def self.export_skus
+    path = "#{Rails.root}/lib/export/vendor_data.csv"
+    CSV.open(path, 'wb') do |csv|
+      csv << [ 'sku', 'hd_url', 'hd_price',
+               'wayfair url', 'wayfair price',
+               'menards url', 'menard price',
+               'overstock url', 'overstock price',
+               'houzz url', 'houzz price']
+      Listing.all.map do |listing|
+        csv << [ listing.sku,
+                 listing.vendors[:hd][:url],
+                 listing.vendors[:hd][:price],
+                 listing.vendors[:wayfair][:url],
+                 listing.vendors[:wayfair][:price],
+                 listing.vendors[:menards][:url],
+                 listing.vendors[:menards][:price],
+                 listing.vendors[:overstock][:url],
+                 listing.vendors[:overstock][:price],
+                 listing.vendors[:houzz][:url],
+                 listing.vendors[:houzz][:price]]
+      end
+    end
   end
 
 end
