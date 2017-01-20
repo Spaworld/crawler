6d99a9f0b2923b4922832aa43cf4b5f8b0d277ee
dimanyc <dimitrynazarov@gmail.com>
2017-01-19 21:02:08 -0500

    inits Overstock connector refactoring

--- app/connectors/overstock.rb
+++ app/connectors/overstock.rb
@@ -1,4 +1,5 @@
 class Overstock < BaseConnector
+
   attr_reader :listing_attrs
 
   BASE_URL = 'https://www.overstock.com/'
@@ -9,10 +10,29 @@ class Overstock < BaseConnector
   def process_listing(node)
     id = node.id
     visit_home_page
-    return if page_not_found?
     search_for_product(id)
     fetch_product_attributes(id)
     store_product_attributes(id)
+  end
+
+  # Visits the home page
+  # raises page_not_found if
+  # connection to host cannot
+  # be established
+  def visit_home_page
+    begin
+    return if driver.current_url == BASE_URL
+    driver.visit(BASE_URL)
+    rescue PageNotFound
+      Notifier.raise_page_not_found
+      return
+    end
+    # raise PageNotFound if page_not_found?
+  end
+
+  def search_for_product(overstock_id)
+    driver.fill_in('keywords', with: overstock_id)
+    driver.first('.os-icon-magnify').click
   end
 
   # def process_listing(node, index)
@@ -34,8 +54,8 @@ class Overstock < BaseConnector
   #     vendor_sku,
   #     vendor_url)
   #   Listing.append_vendor_attrs(
-      # vendor_sku,
-      # @listing_attrs)
+  # vendor_sku,
+  # @listing_attrs)
   # end
 
   # def process_listings(nodes)
@@ -48,38 +68,25 @@ class Overstock < BaseConnector
   #     puts "=== starting id: #{id} \
   #     | sku: #{sku} \
   #     | iteration: #{index}"
-      # search_for_product(id)
-      # if page_not_found? || @listing_attrs.nil?
-      #   puts '>>> page not found. Skipping'
-      #   Listing.append_not_found('overstock', sku, id)
-      #   next
-      # end
-      # fetch_product_attributes(id, sku)
-      # vendor_sku = @listing_attrs[:vendor_sku]
-      # vendor_url = @listing_attrs[:vendor_url]
-      # Listing.append_menards_url(
-        # vendor_sku,
-        # vendor_url)
-      # Listing.append_vendor_attrs(
-        # vendor_sku,
-        # @listing_attrs)
-    # end
+  # search_for_product(id)
+  # if page_not_found? || @listing_attrs.nil?
+  #   puts '>>> page not found. Skipping'
+  #   Listing.append_not_found('overstock', sku, id)
+  #   next
+  # end
+  # fetch_product_attributes(id, sku)
+  # vendor_sku = @listing_attrs[:vendor_sku]
+  # vendor_url = @listing_attrs[:vendor_url]
+  # Listing.append_menards_url(
+  # vendor_sku,
+  # vendor_url)
+  # Listing.append_vendor_attrs(
+  # vendor_sku,
+  # @listing_attrs)
+  # end
   # end
 
-  def visit_home_page
-    return if driver.current_url == BASE_URL
-    driver.visit(BASE_URL)
-  end
 
-  def search_for_product(overstock_id)
-    # begin
-      driver.fill_in('keywords', with: overstock_id)
-      driver.first('.os-icon-magnify').click
-    # rescue
-    #   sleep(rand(1..2))
-    #   search_for_product(overstock_id)
-    # end
-  end
 
   def fetch_product_attributes(overstock_id, sku)
     @listing_attrs =
@@ -104,7 +111,7 @@ class Overstock < BaseConnector
 
   def page_not_found?
     if driver.doc.at('.whoops').present? ||
-      driver.doc.at('.autocorrect-msg').present?
+        driver.doc.at('.autocorrect-msg').present?
       Notifier.raise_page_not_found
       return true
     end
